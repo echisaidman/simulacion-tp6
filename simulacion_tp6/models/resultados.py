e787209c2ba73da3fe05aa6e52d3aee97b535451
Ezequiel Saidman <esaidman@frba.utn.edu.ar>
2024-09-28 06:26:17 +0000

    Primera version para redondear resultados a 3 decimales

--- simulacion_tp6/models/resultados.py
+++ simulacion_tp6/models/resultados.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import Any, cast
 
 
 @dataclass
@@ -10,3 +11,14 @@ class Resultados:
     PARR: float
     PTOJ: list[float]
     PTOS: list[float]
+
+    def __getattribute__(self, name: str) -> Any:
+        value = super().__getattribute__(name)
+        if isinstance(value, float):
+            value = round(value, 3)
+        elif isinstance(value, list):
+            value = cast(list[Any], value)
+            if len(value) > 0 and isinstance(value[0], float):
+                value = cast(list[float], value)
+                value = [round(v, 3) for v in value]
+        return value
